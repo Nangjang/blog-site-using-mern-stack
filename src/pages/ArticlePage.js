27a2599e02339f9fd61dba1a5daa4e5093758ed8
Nangjang <jintrade23@gmail.com>
2024-10-27 19:44:56 +0900

    Add Article Page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,24 @@
+import { useParams } from 'react-router-dom';
+
+import articles from './article-content';
+
 const ArticlePage = () => {
+    // Extract the articleId parameter from the URL
+    const { articleId } = useParams();
+
+    // Find the article that matches the articleId
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>This is the article page!</h1>
+        <>
+            {/* Display the article title */}
+            <h1>{article.title}</h1>
+            
+            {/* Map through the article's content and display each paragraph */}
+            {article.content.map(paragraph => (
+                <p>{paragraph}</p> // Render each paragraph
+            ))}
+        </>
     );
 }
 
