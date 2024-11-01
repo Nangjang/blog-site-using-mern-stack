cbb0f089deba81cf2ebb3296619ddcd77eb10bdb
Nangjang <jintrade23@gmail.com>
2024-11-01 12:38:23 +0900

    Add Article Upvote Feature

--- blog-site-backend/src/server.js
+++ blog-site-backend/src/server.js
@@ -1,7 +1,20 @@
 import express from 'express';
 
 const app = express();
-
+let articlesInfo = [
+    {
+        name: "learn-react",
+        upvotes: 0
+    },
+    {
+        name: "learn-node",
+        upvotes: 0
+    },
+    {
+        name: "mongodb",
+        upvotes: 0
+    },
+];
 app.use(express.json());
 
 app.post('/hello', (req, res) => {
@@ -14,6 +27,19 @@ app.get('/hello/:name', (req, res) => {
 
     const { name } = req.params;
     res.send(`Hello! ${name}`);
+});
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+
+    const article = articlesInfo.find(a => a.name === name);
+    if (!article) {
+        res.send('The article doesn\'t exists.');
+    }
+    else {
+        article.upvotes += 1;
+        res.send(`The article has ${article.upvotes} upvotes.`);
+    }
 });
 
 app.listen(8000, () => {
